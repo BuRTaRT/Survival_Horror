ce654402c5aa005d2caa1f9ad6e84d7c77dea460
Artem <burtart@ukr.net>
2019-02-03 14:17:37 +0200

    fixes

---
+++ js/preloader.js
@@ -0,0 +1,4 @@
+ var preloader = document.getElementById("preloader");
+    window.onload = function() {
+      preloader.style.display = "none";
+    }
--- main.js
+++ main.js
@@ -1,33 +1,34 @@
  let span1 = $(".light1");
 
  function toggler() {
-     span1.toggleClass("light-off");
+   span1.toggleClass("light-off");
  }
  (function loop() {
-     let rand = Math.random() * 1500;
-     setTimeout(function () {
-         toggler();
-         loop();
-     }, rand)
+   let rand = Math.random() * 1500;
+   setTimeout(function () {
+     toggler();
+     loop();
+   }, rand)
  })()
 
  let span2 = $(".light2");
 
  function toggler2() {
-     span2.toggleClass("light-off");
+   span2.toggleClass("light-off");
  }
  (function loop() {
-     let rand = Math.random() * 5000;
-     setTimeout(function () {
-         toggler2();
-         loop();
-     }, rand)
+   let rand = Math.random() * 5000;
+   setTimeout(function () {
+     toggler2();
+     loop();
+   }, rand)
  })()
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+ $(document).on('click', 'a[href^="#"]', function (event) {
+   event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
+   $('html, body').animate({
+     scrollTop: $($.attr(this, 'href')).offset().top
+   }, 500);
+ });
+
